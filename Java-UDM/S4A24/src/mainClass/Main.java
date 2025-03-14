f761d8fcb2cc4e5fd1ee5dea4cf60e22300f9ce1
Victor-Gbarbosa <victor.gbarbosa05@gmail.com>
2025-03-13 21:22:39 -0300

    Primeiro Exemplo do curso de Java da Udemy

---
+++ Java-UDM/S4A24/src/mainClass/Main.java
@@ -0,0 +1,14 @@
+package mainClass;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int variavel = 10;
+		int outraVariavel = 12;
+		int resultado = variavel + outraVariavel;
+		
+		System.out.printf("Valora da soma das variavels = %d", resultado);
+	}
+
+}
