e8e328504d03aaf7f87c87e4362c26e660c310bb
Victor-Gbarbosa <victor.gbarbosa05@gmail.com>
2025-03-27 18:52:54 -0300

    test
    CommitTest

--- Java-UDM/POO_Ex9/src/entities/Client.java
+++ Java-UDM/POO_Ex9/src/entities/Client.java
@@ -1,19 +1,30 @@
 package entities;
 
-import java.util.Date;
 import java.util.Scanner;
 
 public class Client {
     private String name;
     private String email;
-    private Date birthDate;
+    private String birthDate;
 
     Scanner scan = new Scanner(System.in);
 
     public void clientRegister() {
+        System.out.print("\nInsira o nome do cliente: ");
+        this.name = scan.nextLine();
         System.out.print("\nInsira o email do cliente: ");
         this.email = scan.nextLine();
+        System.out.print("\nInsira a data de nascimento do cliente: ");
+        this.email = scan.nextLine();
     }
-}
+
+    public void ShowInfo() {
+        System.out.println("\nNome: " + this.name);
+        System.out.println("Email: " + this.email);
+        System.out.println("Data de nascimento: " + this.birthDate);
+    }
+       
+    }
+
 
 
